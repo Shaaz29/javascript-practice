c2fc9a00833d6fe00eba02629022e0cb4cad89ee
Shaaz29 <ahmadshaaz2910@gmail.com>
2026-06-29 11:02:18 +0530

    Added JavaScript practice assignments

---
+++ Basic Javascript practice assignment/app2.js
@@ -0,0 +1,82 @@
+// Assignment ques 1:
+
+// let n = 45;
+// if(n%10===0){
+//     console.log("Good");
+// }
+// else{
+//     console.log("Bad");
+// }
+
+
+
+// Assignment ques 2:
+
+// let quarter=3;
+
+// switch(quarter){
+//     case 1:
+//         console.log("January , February, March");
+//         break;
+//      case 2:
+//         console.log("April , May, June");
+//         break;
+//      case 3:
+//         console.log("July , August, September");
+//         break;
+//      case 4:
+//         console.log("October , November, December");
+//         break;   
+// }
+
+
+// Assignment ques 3:
+
+// let str="Ahmad Shaaz";
+//  if(((str[0]==="a") ||(str[0]==="A"))&&(str.length>5)){
+//     console.log("Golden String");
+//  }
+//  else{
+//     console.log("Not Golden String");
+//  }
+
+
+// Assignment ques 4:
+
+// let n = 103;
+// let m = 202;
+
+// l = n%10;
+// k = m%10;
+
+// if(l==k){
+//     console.log("Both Numbers have same last digit");
+// }
+// else{
+//     console.log("Both Numbers do not have  same last digit");
+// }
+
+
+
+// Assignment ques 5:
+
+// let a =15;
+// let b =10;
+// let c =5;
+
+// if(a>b && a>c){
+//     console.log("a is largest number");
+// }
+// else if(b>a && b>c){
+//     console.log("b is largest number");
+// }
+// else{
+//     console.log("c is largest number");
+// }
+
+
+// Assignment ques 6:
+
+let name = prompt("Enter Name");
+let age  =  prompt("Enter age");
+alert(name+" is "+age+" years old");
--- app.js
+++ app.js
@@ -1,2 +1,45 @@
-console.log("Hello World!");
-console.log("Apna College");
+// console.log("Hello World!");
+// console.log("Ahmad Shaaz");
+
+// let size=`L`;
+// if(size==`XL`){
+//     console.log("price is Rs. 250");
+// }
+// else if(size==`L`){
+//     console.log("price is Rs. 200");
+// }
+// else if(size==`M`){
+//     console.log("price is Rs. 100");
+// }
+// else if(size==`S`){
+//     console.log("price is Rs. 50");
+// }
+
+
+let day = 3;
+
+switch(day){
+    case 1:
+        console.log("Monday");
+        break;
+    case 2: 
+        console.log("Tuesday");
+        break;
+    case 3: 
+        console.log("Wednesday");
+        break;
+    case 4: 
+        console.log("Thursday");
+        break;
+    case 5: 
+        console.log("Friday");
+        break;
+    case 6: 
+        console.log("Saturday");
+        break;
+    case 7: 
+        console.log("Sunday");
+        break;
+    default:
+        console.log("Wrong Day!");
+}
