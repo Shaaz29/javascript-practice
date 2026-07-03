d247f3c1658915f77eaa5461b3291ef95316a78a
Shaaz29 <ahmadshaaz2910@gmail.com>
2026-07-03 20:40:00 +0530

    new ques solved

--- functions.js
+++ functions.js
@@ -3,3 +3,24 @@ function printName(name) {
 }
 
 printName("Ahmad");
+
+
+// Ques: To find average of 3 numbers by creating a function.
+
+function average(a,b,c){
+    console.log((a+b+c)/3);
+}
+
+average(10, 20, 30);
+
+
+
+// Ques: To print the multiplication table of a number by creating a function.
+
+function multitable(num){
+    for(let i=num; i<=num*10; i=i+num){
+        console.log(i);
+    }
+}
+
+multitable(5);
