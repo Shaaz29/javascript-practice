e0c33afc90380a259937346740764252b937b6de
Shaaz29 <ahmadshaaz2910@gmail.com>
2026-07-04 16:39:49 +0530

    new practice js questions

--- functions.js
+++ functions.js
@@ -24,3 +24,39 @@ function multitable(num){
 }
 
 multitable(5);
+
+
+// Ques: To calculate the sum of numbers from 1 to n by creating a function.
+function sum(n){
+    let total=0;
+    for (let i=1;i<=n; i++){
+        total=total+i; 
+}
+    return total;
+}
+
+
+// Function Scope:
+
+let greet = "Hello"; // global scope
+
+function changeGreet() {
+    let greet = "How are you?"; // function scope
+    console.log(greet); 
+
+    function innerGreet() {
+        console.log(greet); // lexical scope 
+}
+
+innerGreet();
+}
+console.log(greet); // global scope
+changeGreet();
+
+
+
+// Function Expressions:
+
+let sum = function(a, b) {
+    return a + b;
+}
