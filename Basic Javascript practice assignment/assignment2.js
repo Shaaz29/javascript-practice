fa02ddc1413caa7cd6a1b3dcff8fdc8e57e37fe7
Shaaz29 <ahmadshaaz2910@gmail.com>
2026-07-04 19:45:28 +0530

    new question solved

---
+++ Basic Javascript practice assignment/assignment2.js
@@ -0,0 +1,5 @@
+// Question 1: Create a program that generates a random number representing a dice roll.
+//  [The number should be between 1 and 6].
+
+let diceRoll=Math.floor(Math.random()*6)+1;
+console.log(diceRoll);
