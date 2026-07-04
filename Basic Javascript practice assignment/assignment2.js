f7eeca6ece7fcf33ad2464d47ec7b5c2a6428148
Shaaz29 <ahmadshaaz2910@gmail.com>
2026-07-04 20:20:51 +0530

    new questions solved

--- Basic Javascript practice assignment/assignment2.js
+++ Basic Javascript practice assignment/assignment2.js
@@ -3,3 +3,32 @@
 
 let diceRoll=Math.floor(Math.random()*6)+1;
 console.log(diceRoll);
+
+
+// Question 2: Create an object representing a car that stores the following properties for the
+// car: name, model, color.
+// Print the car’s name. 
+
+const car={
+    name: "Maruti Suzuki",
+    model: "Maruti Suzuki Dzire",
+    color: "White",
+};
+console.log(car.name);
+
+
+
+
+// Question 3: Create an object Person with their name, age and city.
+// Edit their city’s original value to change it to “NewYork”.
+// Add a new property country and set it to the United States.
+
+const person={
+    name: "Ahmad Shaaz",
+    age: 21,
+    city: "New Jersey",
+};
+
+person.city="New York";
+person.country="United States";
+console.log(person);
