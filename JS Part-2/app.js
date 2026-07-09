6687954d33eea6e7f1b6831c0e323b5a30d0f181
Shaaz29 <ahmadshaaz2910@gmail.com>
2026-07-09 16:55:23 +0530

    new practice

---
+++ JS Part-2/app.js
@@ -0,0 +1,10 @@
+console.dir(document.querySelector("h1"));
+
+console.dir(document.querySelector("#description"));
+
+console.dir(document.querySelector(".oldImg"));
+
+
+console.dir(document.querySelector("div a "));
+
+console.dir(document.querySelectorAll("div a "));
