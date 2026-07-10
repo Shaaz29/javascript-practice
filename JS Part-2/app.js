08576e7b5bc8a6bff177a34d825311fb38812041
Shaaz29 <ahmadshaaz2910@gmail.com>
2026-07-10 12:15:49 +0530

    new changes

--- JS Part-2/app.js
+++ JS Part-2/app.js
@@ -8,3 +8,15 @@ console.dir(document.querySelector(".oldImg"));
 console.dir(document.querySelector("div a "));
 
 console.dir(document.querySelectorAll("div a "));
+
+
+// For inserting a new paragraph or anything use this:
+
+let newP = document.createElement('p');
+
+newP.innerText="Hi this is new paragraph";
+
+
+let body = document.querySelector('body');
+
+body.appendChild(newP);
