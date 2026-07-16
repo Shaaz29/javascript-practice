edfcd57e9e3fc3603d8b63783489640aaaaabf51
Shaaz29 <ahmadshaaz2910@gmail.com>
2026-07-16 08:32:02 +0530

    api usage

--- JS Part-5/app8.js
+++ JS Part-5/app8.js
@@ -19,14 +19,82 @@
 // Using Fetch with async and await:
 
 
-let url = "https://catfact.ninja/fact";
+// let url = "https://catfact.ninja/fact";
 
-async function getFacts() {
-    try{
-        let res = await fetch(url);
-        let data = await res.json();
-        console.log(data.fact);
-    } catch(err){
-        console.log("error - ",err);
-    } 
+// async function getFacts() {
+//     try{
+//         let res = await fetch(url);
+//         let data = await res.json();
+//         console.log(data.fact);
+//     } catch(err){
+//         console.log("error - ",err);
+//     } 
+// }
+
+
+
+
+
+
+// Same code using Axios:
+
+// let btn = document.querySelector("button");
+// btn.addEventListener("click", async () => {
+//     let fact = await getFacts();
+//     // console.log(fact);
+//     let p = document.querySelector("#result");
+//     p.innerText = fact;
+// });
+
+
+// let url = "https://catfact.ninja/fact";
+
+// async function getFacts() {
+//     try{
+//         let res = await axios.get(url);
+//         return res.data.fact;
+//     } catch(err){
+//         console.log("error - ",err);
+//         return "No fact Found";
+//     } 
+// }
+
+
+
+
+
+
+
+
+let url = "http://universities.hipolabs.com/search?name="
+
+let btn = document.querySelector("button");
+btn.addEventListener("click", async() => {
+    let country = document.querySelector("input").value;
+    console.log(country);
+    let colArr = await getColleges(country);
+    show(colArr);
+});
+
+function show(colArr){
+    let list = document.querySelector("#list");
+    list.innerText="";
+    for(col of colArr){
+        console.log(col.name);
+
+        let li = document.createElement("li");
+        li.innerText = col.name;
+        list.appendChild(li);
+    }
 }
+
+async function getColleges(country){
+    try{
+        let res = await axios.get(url+country);
+        return res.data;
+    }catch(err){
+        console.log("error :",err);
+        return [];
+    }
+}
+
